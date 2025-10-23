b0e6b1b5bdf2fff09d164365c7122bc14340bc5f
Entiti3039086 <155929120+Entiti3039086@users.noreply.github.com>
2025-10-23 14:21:49 +0200

    Delete vscode/TEST directory

--- vscode/TEST/get back here.js
+++
@@ -1,7 +0,0 @@
-let doctitle = document.title;
-window.addEventListener("blur", () => {
-    document.title = "get back here";
-});
-window.addEventListener("focus", () => {
-    document.title = doctitle;
-});
